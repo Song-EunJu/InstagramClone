524c50d4ee508fee96ab7cd594d0fceed5160aec
Song-EunJu <jd06280@naver.com>
2021-08-03 22:24:22 +0900

    [ADD] 사진 업로드 페이지

--- app/src/main/java/com/example/eunjuinstagram/MainActivity.kt
+++ app/src/main/java/com/example/eunjuinstagram/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.eunjuinstagram
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.eunjuinstagram.navigation.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+                // add_photo activity 를 호출할 수 있는 코드
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) { //외부 스토리지 권한을 가져올 수 있는지 체크
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                }
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                return true
+            }
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+        // 사진 경로를 가져올 수 있는 권한 요청
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
     }
+
+
 }
---
+++ app/src/main/java/com/example/eunjuinstagram/navigation/AddPhotoActivity.kt
@@ -0,0 +1,63 @@
+package com.example.eunjuinstagram.navigation
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.eunjuinstagram.R
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_photo.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddPhotoActivity : AppCompatActivity() {
+    var PICK_IMAGE_FROM_ALBUM = 0
+    var storage : FirebaseStorage? = null
+    var photoUri : Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photo)
+
+        // Initiate storage
+        storage = FirebaseStorage.getInstance()
+
+        // Open the album
+        var photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
+
+        // add image upload event
+        addphoto_btn_upload.setOnClickListener {
+            contentUpload()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == PICK_IMAGE_FROM_ALBUM){
+            if(resultCode == Activity.RESULT_OK){
+                // 사진을 선택했을 때 이미지의 경로가 여기로 넘어오게 된다.
+                photoUri = data?.data
+                addphoto_image.setImageURI(photoUri)
+            }
+            else{
+                // 취소 버튼을 눌렀을 때 작동
+                finish()
+            }
+        }
+    }
+    fun contentUpload(){
+        // 파일 이름 생성 - 랜덤생성하기 위해 날짜 값 사용
+        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imageFileName = "IMAGE_" + timestamp + "_.png"
+        var storageRef = storage?.reference?.child("images")?.child(imageFileName)
+
+        // 파일 업로드
+        storageRef?.putFile(photoUri!!)?.addOnSuccessListener{
+            Toast.makeText(this, getString(R.string.upload_success),Toast.LENGTH_LONG).show()
+        }
+    }
+}
