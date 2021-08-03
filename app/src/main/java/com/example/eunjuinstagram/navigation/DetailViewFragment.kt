1bafa4285e6e7a77d034016ddc372da1d510a894
Song-EunJu <jd06280@naver.com>
2021-08-03 11:57:26 +0900

    [ADD] 네비게이션 바 추가

---
+++ app/src/main/java/com/example/eunjuinstagram/navigation/DetailViewFragment.kt
@@ -0,0 +1,9 @@
+package com.example.eunjuinstagram.navigation
+
+import androidx.fragment.app.Fragment
+
+class DetailViewFragment : Fragment(){
+
+
+}
+git remote set-url origin https://github.com/Song-EunJu/InstagramClone.git
